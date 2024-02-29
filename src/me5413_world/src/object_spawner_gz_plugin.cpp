279088f203000df8fd086c396a3102a6498da1ae
SS47816 <ss1057532022@gmail.com>
2024-02-29 14:17:22 +0800

    feat: code clean up

--- src/me5413_world/src/object_spawner_gz_plugin.cpp
+++ src/me5413_world/src/object_spawner_gz_plugin.cpp
@@ -12,124 +12,89 @@ class ObjectSpawner : public WorldPlugin
 public:
   void Load(physics::WorldPtr _parent, sdf::ElementPtr /*_sdf*/)
   {
-    // Option 1: Insert model from file via function call.
-    // The filename must be in the GAZEBO_MODEL_PATH environment variable.
-    //_parent->InsertModelFile("model://box");
+    // Create a new transport node
+    transport::NodePtr node(new transport::Node());
 
-    // Option 2: Insert model from string via function call.
-    // Insert a sphere model from string
+    // Initialize the node with the world name
+    node->Init(_parent->Name());
 
-    // sdf::SDF sphereSDF;
-    // sphereSDF.SetFromString(
-    //    "<sdf version ='1.4'>\
-    //       <model name ='sphere'>\
-    //         <pose>10 0 0 0 0 0</pose>\
-    //         <link name ='link'>\
-    //           <pose>0 0 .5 0 0 0</pose>\
-    //           <collision name ='collision'>\
-    //             <geometry>\
-    //               <sphere><radius>0.5</radius></sphere>\
-    //             </geometry>\
-    //           </collision>\
-    //           <visual name ='visual'>\
-    //             <geometry>\
-    //               <sphere><radius>0.5</radius></sphere>\
-    //             </geometry>\
-    //           </visual>\
-    //         </link>\
-    //       </model>\
-    //     </sdf>");
-    // // Demonstrate using a custom model name.
-    // sdf::ElementPtr model = sphereSDF.Root()->GetElement("model");
-    // model->GetAttribute("name")->SetFromString("unique_sphere");
-    // _parent->InsertModelSDF(sphereSDF);
+    // Create a publisher on the ~/factory topic
+    transport::PublisherPtr factoryPub = node->Advertise<msgs::Factory>("~/factory");
 
-    // Option 3: Insert model from file via message passing.
+    // Create the message
+    msgs::Factory box_1_msg;
+    msgs::Factory box_2_msg;
+    msgs::Factory box_3_msg;
+    msgs::Factory cone_msg;
+
+    /*********************************** Generate Random Boxes **********************************/
+    // Seed for random number generation
+    std::srand(std::time(0));
+    // Generate random points within a 10 by 8 area with a distance greater than 4.3
+    ignition::math::Vector2d p1, p2, p3;
+    do
     {
-      // Create a new transport node
-      transport::NodePtr node(new transport::Node());
+      p1 = ignition::math::Vector2d(
+        static_cast<double>(std::rand()) / RAND_MAX * 8.0 + 8,
+        1 - static_cast<double>(std::rand()) / RAND_MAX * 7.25);
 
-      // Initialize the node with the world name
-      node->Init(_parent->Name());
+      p2 = ignition::math::Vector2d(
+        static_cast<double>(std::rand()) / RAND_MAX * 8.0 + 8,
+        -6.25 + static_cast<double>(std::rand()) / RAND_MAX * 7.25);
 
-      // Create a publisher on the ~/factory topic
-      transport::PublisherPtr factoryPub = node->Advertise<msgs::Factory>("~/factory");
+      p3 = ignition::math::Vector2d(
+        static_cast<double>(std::rand()) / RAND_MAX * 8.0 + 8,
+        -6.25 + static_cast<double>(std::rand()) / RAND_MAX * 7.25);
+    } while ((p1 - p2).Length() <= 4.3 ||
+              (p1 - p3).Length() <= 4.3 ||
+              (p2 - p3).Length() <= 4.3 ||
+              (p1.X() > 16.2 && p1.X() < 16.9 && p1.Y() > -6.16 && p1.Y() < -5.76) ||
+              (p2.X() > 16.2 && p2.X() < 16.9 && p2.Y() > -6.16 && p2.Y() < -5.76) ||
+              (p3.X() > 16.2 && p3.X() < 16.9 && p3.Y() > -6.16 && p3.Y() < -5.76));
 
-      // Create the message
-      msgs::Factory box_1_msg;
-      msgs::Factory box_2_msg;
-      msgs::Factory box_3_msg;
-      msgs::Factory cone_msg;
+    // Model file to load
+    box_1_msg.set_sdf_filename("model://number1");
+    box_2_msg.set_sdf_filename("model://number2");
+    box_3_msg.set_sdf_filename("model://number3");
+    cone_msg.set_sdf_filename("model://construction_cone");
 
-      // Generate random points within a 10 by 8 area with a distance greater than 4.3
-      ignition::math::Vector2d p1, p2, p3;
+    // Pose to initialize the model to
+    msgs::Set(box_1_msg.mutable_pose(),
+              ignition::math::Pose3d(
+                  ignition::math::Vector3d(p1.X(), p1.Y(), 0.4),
+                  ignition::math::Quaterniond(0, 0, 0)));
 
-      do
-      {
-        p1 = ignition::math::Vector2d(
-          static_cast<double>(std::rand()) / RAND_MAX * 8.0 + 8,
-          1 - static_cast<double>(std::rand()) / RAND_MAX * 7.25);
+    msgs::Set(box_2_msg.mutable_pose(),
+              ignition::math::Pose3d(
+                  ignition::math::Vector3d(p2.X(), p2.Y(), 0.4),
+                  ignition::math::Quaterniond(0, 0, 0)));
 
-        p2 = ignition::math::Vector2d(
-          static_cast<double>(std::rand()) / RAND_MAX * 8.0 + 8,
-          -6.25 + static_cast<double>(std::rand()) / RAND_MAX * 7.25);
+    msgs::Set(box_3_msg.mutable_pose(),
+              ignition::math::Pose3d(
+                  ignition::math::Vector3d(p3.X(), p3.Y(), 0.4),
+                  ignition::math::Quaterniond(0, 0, 0)));
 
-        p3 = ignition::math::Vector2d(
-          static_cast<double>(std::rand()) / RAND_MAX * 8.0 + 8,
-          -6.25 + static_cast<double>(std::rand()) / RAND_MAX * 7.25);
-      } while ((p1 - p2).Length() <= 4.3 ||
-               (p1 - p3).Length() <= 4.3 ||
-               (p2 - p3).Length() <= 4.3 ||
-               (p1.X() > 16.2 && p1.X() < 16.9 && p1.Y() > -6.16 && p1.Y() < -5.76) ||
-               (p2.X() > 16.2 && p2.X() < 16.9 && p2.Y() > -6.16 && p2.Y() < -5.76) ||
-               (p3.X() > 16.2 && p3.X() < 16.9 && p3.Y() > -6.16 && p3.Y() < -5.76));
-
-      // Model file to load
-      box_1_msg.set_sdf_filename("model://number1");
-      box_2_msg.set_sdf_filename("model://number2");
-      box_3_msg.set_sdf_filename("model://number3");
-      cone_msg.set_sdf_filename("model://construction_cone");
-
-      // Seed for random number generation
-      std::srand(std::time(0));
-
-      if (std::rand() % 2 == 0)
-      {
-        msgs::Set(cone_msg.mutable_pose(),
-                  ignition::math::Pose3d(
-                      ignition::math::Vector3d(12.7, 2.5, 0.4),
-                      ignition::math::Quaterniond(0, 0, 0)));
-      }
-      else
-      {
-        msgs::Set(cone_msg.mutable_pose(),
-                  ignition::math::Pose3d(
-                      ignition::math::Vector3d(16.9, 2.5, 0.4),
-                      ignition::math::Quaterniond(0, 0, 0)));
-      }
-
-      // Pose to initialize the model to
-      msgs::Set(box_1_msg.mutable_pose(),
+    /*********************************** Generate Traffic Cone **********************************/
+    if (std::rand() % 2 == 0)
+    {
+      msgs::Set(cone_msg.mutable_pose(),
                 ignition::math::Pose3d(
-                    ignition::math::Vector3d(p1.X(), p1.Y(), 0.4),
+                    ignition::math::Vector3d(12.7, 2.5, 0.4),
                     ignition::math::Quaterniond(0, 0, 0)));
-
-      msgs::Set(box_2_msg.mutable_pose(),
-                ignition::math::Pose3d(
-                    ignition::math::Vector3d(p2.X(), p2.Y(), 0.4),
-                    ignition::math::Quaterniond(0, 0, 0)));
-
-      msgs::Set(box_3_msg.mutable_pose(),
-                ignition::math::Pose3d(
-                    ignition::math::Vector3d(p3.X(), p3.Y(), 0.4),
-                    ignition::math::Quaterniond(0, 0, 0)));
-
-      // Send the message
-      factoryPub->Publish(box_1_msg);
-      factoryPub->Publish(box_2_msg);
-      factoryPub->Publish(box_3_msg);
-      factoryPub->Publish(cone_msg);
     }
+    else
+    {
+      msgs::Set(cone_msg.mutable_pose(),
+                ignition::math::Pose3d(
+                    ignition::math::Vector3d(16.9, 2.5, 0.4),
+                    ignition::math::Quaterniond(0, 0, 0)));
+    }
+
+    // Send messages
+    factoryPub->Publish(box_1_msg);
+    factoryPub->Publish(box_2_msg);
+    factoryPub->Publish(box_3_msg);
+    factoryPub->Publish(cone_msg);
   }
 };
 
